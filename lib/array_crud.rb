f9618a7f476fc6f6a8f87b49266998f34c35c94a
Justin Lupica <justinlupica@gmail.com>
2020-07-10 17:19:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  names = ["Justin", "Jack", "Joe", "James"]
+  array = ["Justin", "Jack", "Joe", "James"]
 end
 
 def add_element_to_end_of_array(array, element)
-  names.push("JGOD")
+  array.push("JGOD")
 end
 
 def add_element_to_start_of_array(array, element)
